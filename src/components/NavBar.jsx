3aee26904599ec04cf3ebffbf163c9cdf3c14d31
Praveen Perumal <ppraveen98841@gmail.com>
2022-03-21 12:26:23 +0530

    Added JS functionality
    Added show menu & slide functionality to the project

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,13 @@
+import React, { useState } from 'react';
+
 function NavBar(props) {
+  const [show, setShow] = useState(false);
+
   return (
-    <div className="navbar">
+    <div className={`navbar ${show ? "navbar--show" : ""}`}>
       <div className="toggle-navbar">
-        <img src="images/svg/hamburger.svg" alt="hamburger menu" />
-        <img src="images/svg/close.svg" alt="close menu" />
+        <img onClick={() => setShow(true)} src="images/svg/hamburger.svg" alt="hamburger menu" />
+        <img onClick={() => setShow(false)} src="images/svg/close.svg" alt="close menu" />
       </div>
 
       <img src="images/svg/logo.svg" alt="Room's logo" className="logo" />
--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,11 +1,22 @@
+import React, { useState } from 'react';
+
 function Slide(props) {
+  const [slide,changeSlide] = useState(1)
+  const handleClick = (operation) => {
+    if (operation === "+" && slide + 1 < 4) {
+      changeSlide(slide + 1)
+    }else if (operation === "-" && slide - 1 > 0) {
+      changeSlide(slide - 1)
+    }
+  }
+
   return (
-    <div className="slide">
+    <div className="slide" style={{backgroundImage: `url("images/slide/mobile-${slide}.jpg")`}}>
       {/* Note: the image will be set using background-image */}
 
       <div className="slide-btn">
-        <div><svg width="14" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M13 0L1 12l12 12" stroke="#FFF" fill="none" fill-rule="evenodd"/></svg></div>
-        <div><svg width="14" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M1 0l12 12L1 24" stroke="#FFF" fill="none" fill-rule="evenodd"/></svg></div>
+        <div onClick={() => handleClick("-")}><svg width="14" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M13 0L1 12l12 12" stroke="#FFF" fill="none" fillRule="evenodd"/></svg></div>
+        <div onClick={() => handleClick("+")}><svg width="14" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M1 0l12 12L1 24" stroke="#FFF" fill="none" fillRule="evenodd"/></svg></div>
       </div>
     </div>
   );
