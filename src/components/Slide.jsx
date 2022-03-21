5c1d10dcaa59799932f51064e3c4a2b423f89aea
Praveen Perumal <ppraveen98841@gmail.com>
2022-03-21 11:01:23 +0530

    Styled slide section
    completed the styling of the slide section
    Note: Javascript for sliding functionality is not completed!

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -4,8 +4,8 @@ function Slide(props) {
       {/* Note: the image will be set using background-image */}
 
       <div className="slide-btn">
-        <img src="images/svg/angle-left.svg" alt="left slide" />
-        <img src="images/svg/angle-right.svg" alt="right slide" />
+        <div><svg width="14" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M13 0L1 12l12 12" stroke="#FFF" fill="none" fill-rule="evenodd"/></svg></div>
+        <div><svg width="14" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M1 0l12 12L1 24" stroke="#FFF" fill="none" fill-rule="evenodd"/></svg></div>
       </div>
     </div>
   );
